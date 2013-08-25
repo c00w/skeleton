dd2c180352b6990b3b47dc0c6e3fbc14b63749a4
Colin Rice <colin@daedrum.net>
2013-08-25 01:35:06 -0400

    skeleton: Switch to json, do initial implementation

--- src/skeleton/main.go
+++ src/skeleton/main.go
@@ -1,18 +1,42 @@
 package main
 
 import (
-    "fmt"
-    "log"
+    "encoding/json"
     "io/ioutil"
+    "log"
+    "os"
 )
 
+type Container struct {
+    quantity int
+    mode string
+    granularity string
+}
+
+type MachineType struct {
+    provider string
+    ip []string
+}
+
+type SkeletonDeployment struct {
+    machines MachineType
+    containers []Container
+}
+
 func main() {
-    config, err := ioutil.ReadFile("bonesFile")
+    config, err := os.Open("bonesFile")
 
     if err != nil {
         log.Fatal(err)
     }
 
-    fmt.Printf(string(config))
+    configslice, err := ioutil.ReadAll(config)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    var deploy SkeletonDeployment
+
+    json.Unmarshal(configslice, deploy)
 
 }
