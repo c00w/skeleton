d46e0e118ddd427a94eb18d094b96d540f7bc549
Colin Rice <colin@daedrum.net>
2013-08-26 17:59:15 -0400

    skeleton: Run go fmt

--- src/skeleton/main.go
+++ src/skeleton/main.go
@@ -1,67 +1,67 @@
 package main
 
 import (
-    "encoding/json"
-    "io/ioutil"
-    "log"
-    "os"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
 type Container struct {
-    Quantity int
-    Mode string
-    Granularity string
+	Quantity    int
+	Mode        string
+	Granularity string
 }
 
 type MachineType struct {
-    Provider string
-    Ip []string
+	Provider string
+	Ip       []string
 }
 
 type SkeletonDeployment struct {
-    Test string
-    Machines MachineType
-    Containers map[string]Container
+	Test       string
+	Machines   MachineType
+	Containers map[string]Container
 }
 
 func loadBonesFile() *SkeletonDeployment {
 
-    config, err := os.Open("bonesFile")
-    if err != nil {
-        log.Fatal(err)
-    }
+	config, err := os.Open("bonesFile")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    configslice, err := ioutil.ReadAll(config)
-    if err != nil {
-        log.Fatal(err)
-    }
+	configslice, err := ioutil.ReadAll(config)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    deploy := new(SkeletonDeployment)
+	deploy := new(SkeletonDeployment)
 
-    err = json.Unmarshal(configslice, deploy)
-    if err != nil {
-        log.Fatal(err)
-    }
+	err = json.Unmarshal(configslice, deploy)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    for k,v := range deploy.Containers {
-        if len(v.Granularity) == 0 {
-            v.Granularity = "deployment"
-            deploy.Containers[k] = v
-        }
-        if len(v.Mode) == 0 {
-            v.Mode = "default"
-            deploy.Containers[k] = v
-        }
-    }
+	for k, v := range deploy.Containers {
+		if len(v.Granularity) == 0 {
+			v.Granularity = "deployment"
+			deploy.Containers[k] = v
+		}
+		if len(v.Mode) == 0 {
+			v.Mode = "default"
+			deploy.Containers[k] = v
+		}
+	}
 
-    if len(deploy.Machines.Provider) == 0 {
-        log.Fatal("Machine Provider must be specified")
-    }
+	if len(deploy.Machines.Provider) == 0 {
+		log.Fatal("Machine Provider must be specified")
+	}
 
-    return deploy
+	return deploy
 }
 
 func main() {
-    _ = loadBonesFile()
-    log.Print("bonesFile Parsed")
+	_ = loadBonesFile()
+	log.Print("bonesFile Parsed")
 }
--- src/skeleton/main_test.go
+++ src/skeleton/main_test.go
@@ -1,18 +1,19 @@
 package main
+
 import (
-    "testing"
-    "os"
-    "os/exec"
+	"os"
+	"os/exec"
+	"testing"
 )
 
 func TestBonesLoading(t *testing.T) {
-    err := os.Chdir(os.Getenv("GOPATH") + "/test/skeleton")
-    if err != nil {
-        t.Error(err)
-    }
-    cmd := exec.Command(os.Getenv("GOPATH") + "/bin/skeleton")
-    err = cmd.Run()
-    if err != nil {
-        t.Error(err)
-    }
+	err := os.Chdir(os.Getenv("GOPATH") + "/test/skeleton")
+	if err != nil {
+		t.Error(err)
+	}
+	cmd := exec.Command(os.Getenv("GOPATH") + "/bin/skeleton")
+	err = cmd.Run()
+	if err != nil {
+		t.Error(err)
+	}
 }
