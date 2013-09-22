4e87565f4883f427169fc33b2ea557f3b25095ff
Colin Rice <colin@daedrum.net>
2013-09-22 13:51:08 -0400

    test.bat: use windows \

--- Test.bat
+++ Test.bat
@@ -2,9 +2,9 @@ SET GOPATH=%CD%
 go install skeleton
 go install gatekeeper
 go install orchestrator
-copy bin/orchestrator containers/orchestrator/orchestrator
-SET VAGRANT_CWD=%CD%/test
+copy bin\orchestrator containers\orchestrator\orchestrator
+SET VAGRANT_CWD=%CD%\test
 vagrant up
-cd test/skeleton/hello/ && go build
+cd test\skeleton\hello\ && go build
 go test skeleton security orchestrator
 pause
