c4cec82d5d1a2ce6be3f13c704a6e24477d6c56a
Colin Rice <colin@daedrum.net>
2013-09-22 13:36:29 -0400

    test.bat: cp -> copy

--- Test.bat
+++ Test.bat
@@ -2,7 +2,7 @@ SET GOPATH=%CD%
 go install skeleton
 go install gatekeeper
 go install orchestrator
-cp bin/orchestrator containers/orchestrator/orchestrator
+copy bin/orchestrator containers/orchestrator/orchestrator
 SET VAGRANT_CWD=%CD%/test
 vagrant up
 cd test/skeleton/hello/ && go build
