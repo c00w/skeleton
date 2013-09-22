524cf011c9a3bd3b15673f4eb7903d30ef7307c7
Colin Rice <colin@daedrum.net>
2013-09-22 14:22:54 -0400

    test.bat: Do cross compiling for windows

--- Test.bat
+++ Test.bat
@@ -1,10 +1,15 @@
 SET GOPATH=%CD%
 go install skeleton
+SET GOOS=linux
+SET GOPATH=amd64
 go install gatekeeper
 go install orchestrator
 copy bin\orchestrator containers\orchestrator\orchestrator
 SET VAGRANT_CWD=%CD%\test
 vagrant up
 cd test\skeleton\hello\ && go build
-go test skeleton security orchestrator
+SET GOOS=
+SET GOPATH=
+go test skeleton
+go test security orchestrator
 pause
