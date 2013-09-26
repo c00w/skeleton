5c648ec67b72d66204dc3c504c6606342c4af073
Colin Rice <colin@daedrum.net>
2013-09-26 19:13:47 -0400

    test/Vagrantfile: Spin out provisioning and fix it

---
+++ test/provision.sh
@@ -0,0 +1,8 @@
+sh -c "curl https://get.docker.io/gpg | apt-key add -"
+sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+apt-get update
+apt-get install lxc-docker -y
+sed -i 's/docker -d$/docker -d -H=tcp:\\/\\/0.0.0.0/g' /etc/init/docker.conf
+stop docker
+initctl reload docker
+start docker
